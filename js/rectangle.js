17b65d93db7135e828d699cc0f623657736961f1
talfour <jak.malinowski94@gmail.com>
2022-11-04 09:31:29 +0000

    Initial commit

---
+++ js/rectangle.js
@@ -0,0 +1,15 @@
+export function getRectanglePerimeter(length, width) {
+    return 2 * (length + width);
+}
+
+export function getRectangleArea(length, width) {
+    return length * width;
+}
+
+export function getRectangleInfo(length, width) {
+    const area = getRectangleArea(length, width);
+    const perimeter = getRectanglePerimeter(length, width);
+    return console.log(`The perimeter of a rectangle is ${perimeter} and the area is ${area}`)
+}
+
+export default { getRectanglePerimeter, getRectangleArea, getRectangleInfo }
